414f90e57b196c31d462b2fb043275553a6bc518
m85091081 <b10216041@yuntech.edu.tw>
2015-12-25 06:38:56 +0800

    first commit

---
+++ setting.py
@@ -0,0 +1,12 @@
+##Server Port Setting
+port = 8509
+
+##Debug Mode , 0 = off , 1 = on
+debug = 0
+
+## Database Setting
+sqliteFile = '../sqlite/0MuMDAU.db'
+
+## key
+yourkey = 'aowe3k^%^tjke$%^FW'
+
