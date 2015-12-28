e7117f7e78a7271a92d805de546237a26069bdfa
m85091081 <b10216041@yuntech.edu.tw>
2015-12-28 15:50:40 +0800

    add editor

--- muMDAU_app/editor.py
+++ muMDAU_app/editor.py
@@ -1,5 +1,5 @@
-from flask import request , session , Response , render_template , Blueprint , url_for 
-import os , hashlib , subprocess
+from flask import request , session , Response , render_template , Blueprint , redirect , url_for
+import os 
 
 peditor = Blueprint('peditor',__name__)
 markdown = Blueprint('markdown',__name__)
@@ -8,7 +8,7 @@ def edit(username=None):
     if 'username' in session:
         return render_template('redit.html', username = session['username'])
 
-@markdown.route('/submit' , methods=['GET','POST'])
+@markdown.route('/save' , methods=['GET','POST'])
 def save():
     if request.method == "POST":
         argment = request.form['content']
@@ -23,23 +23,43 @@ def save():
             f = open('_posts/'+ filen  +'.markdown', 'wb+') 
             f.write(argment.encode('UTF-8'))
             f.close()
-            ans = "file open"
             return "文章已經存在本地的_posts,重新整理即可在佇列中看到"
 
         
 @markdown.route('/list/<listmd>', methods=['GET','POST'])
 def markdownr(listmd):
     if request.method == "POST":
-        f = open('./_posts/' + listmd)
+        f = open('./markdown/' + listmd)
         return f.read()
     else:
         return "你怎摸不去吃大便"
 
 
+@markdown.route('/del/posts/<listmd>', methods=['GET','POST'])
+def delposts(listmd):
+    if request.method == "POST":
+        filepath = './_posts/' + str(listmd)
+        os.remove(filepath)
+        return "OK"
+    else:
+        return "你怎摸不去吃大便"
+
+
+@markdown.route('/listed/<listposed>', methods=['GET','POST'])
+def markdownrp(listposed):
+    if request.method == "POST":
+        f = open('./blog/_posts/'+ listposed)
+        return f.read()
+    else:
+        return "在try我的後台嘛？你怎摸不去吃大便"
+
 @peditor.route('/<lists>')
 def jsonlist(lists):
     if 'username' in session:
-        postpath = "./_posts"
+        if lists == "page" :
+            postpath = "./markdown"
+        else:
+            postpath = "./blog/_posts"
             
         if os.path.exists(postpath) == True:
             directory = os.path.expanduser(postpath)
@@ -48,17 +68,19 @@ def jsonlist(lists):
             if os.listdir(directory) ==None:
                 return("[""]")
             else:
+                print(len(os.listdir(directory)))
                 for f in os.listdir(directory):
                     if os.path.isfile(os.path.join(directory, f)):
                         i = i + 1
                         data.insert(i,f)
+                        print()
                     import json
                     jsondump = json.dumps(data,separators=( ',' , ':'))
                     resp = Response(response=jsondump,status=200, mimetype="application/json")
-                    return(resp)
+                return(resp)
         else:
             os.makedirs(postpath)
         return 'OK'
     else:
-        return redirect(ur_for(loginp))
+        return redirect(url_for("loginp"))
 
