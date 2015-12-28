2fa7a511673076bf271bc52be11c7c7d2fcdd171
m85091081 <b10216041@yuntech.edu.tw>
2015-12-28 12:49:28 +0800

    add new sql file

--- database.py
+++ database.py
@@ -8,7 +8,7 @@ class InitDB:
         try:
             with sqlite3.connect(sets.sqliteFile) as conn:
                 cursor = conn.cursor()
-                cursor.execute('CREATE TABLE user(username PRIMARY KEY, password, admin)')
+                cursor.execute('CREATE TABLE user(username PRIMARY KEY, password, admin ,flogin)')
                 conn.close
                 return True
         except:
@@ -28,25 +28,49 @@ class LoginSQL:
             password = False 
         return password
 
-
-class ManageSQL:
-    def addUser(user,password,admin):
+class countUSER:
+    def countAdmin():
         try:
             with sqlite3.connect(sets.sqliteFile) as conn:
-                cursor = conn.cursor('INSERT INTO user (username,password,admin) VALUES (?,?,?)',user,password,admin)
-                cursor.execute()
+                cursor = conn.cursor()
+                cursor.execute('select count(*) as numers from user where admin = 1')
+                usercount = cursor.fetchone()
+                conn.close
+        except:
+            conn.close
+            usercount = False 
+        return usercount
+    def countUser():
+        try:
+            with sqlite3.connect(sets.sqliteFile) as conn:
+                cursor = conn.cursor()
+                cursor.execute('select count(*) as numers from user')
+                usercount = cursor.fetchone()
+                conn.close
+        except:
+            conn.close
+            usercount = False 
+        return usercount
+
+
+class ManageSQL:
+    def addUser(user,password,admin,first):
+        try:
+            with sqlite3.connect(sets.sqliteFile) as conn:
+                values=[(str(user),str(password),int(admin),int(first)),]
+                print(conn.executemany('INSERT INTO user VALUES (?,?,?,?)',values))
                 conn.commit()
                 conn.close
                 return True
         except:
             conn.close
-            return False 
+            return False
 
     def setAdmin(username,admin):
         try:
             with sqlite3.connect(sets.sqliteFile) as conn:
-                cursor = conn.cursor('UPDATE user SET admin=? WHERE username=?',admin,username)
-                cursor.execute()
+                values=[(admin,username),]
+                conn.executemany('UPDATE user SET admin=? WHERE username=?',values)
                 conn.commit()
                 conn.close
                 return True
@@ -57,12 +81,37 @@ class ManageSQL:
     def setPassword(username,password):
         try:
             with sqlite3.connect(sets.sqliteFile) as conn:
-                cursor = conn.cursor('UPDATE user SET password=? WHERE username=?',username,username)
-                cursor.execute()
+                values=[(password,username),]
+                conn.executemany('UPDATE user SET password=? WHERE username=?',values)
                 conn.commit()
                 conn.close
                 return True
         except:
             conn.close
             return False 
+
+    def setFirst(username,first):
+        try:
+            with sqlite3.connect(sets.sqliteFile) as conn:
+                cursor = conn.cursor()
+                values=[(first,username),]
+                cursor.execute('UPDATE user SET flogin=? WHERE username=?',values)
+                data=cursor.fetchall()
+                conn.close
+                return data
+        except:
+            conn.close
+            return False
+
+    def listUser():
+        try:
+            with sqlite3.connect(sets.sqliteFile) as conn:
+                cursor = conn.cursor()
+                cursor.execute('select username from user')
+                data=cursor.fetchall()
+                conn.close
+                return data
+        except:
+            conn.close
+            return False
 
