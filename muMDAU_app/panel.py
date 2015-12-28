ebceb09733575939d7a3f90b9095a41e6b8f273c
m85091081 <b10216041@yuntech.edu.tw>
2015-12-28 13:07:15 +0800

    add init

--- muMDAU_app/panel.py
+++ muMDAU_app/panel.py
@@ -1,8 +1,6 @@
 from muMDAU_app import app
+from database import countUSER , ManageSQL 
 from flask import request , session , render_template , url_for ,redirect
-import sqlite3 
-import setting
-import os
 @app.route('/admin' , methods=['GET','POST'])
 def panel():
     if request.method == "POST":
@@ -11,5 +9,24 @@ def panel():
         if 'username' in session:
             return render_template('panel.html', username = session['username'])
         else:
-            return redirect(url_for('loginp'))
+            answer = countUSER.countAdmin()
+            print(answer)
+            if answer[0] == 0 :
+                return redirect(url_for('init'))
+            else:
+                return redirect(url_for('loginp'))
+
+@app.route('/init', methods=['GET', 'POST'])
+def init():
+    if request.method == 'POST':
+        user = request.form['buser']
+        passd = request.form['bpass']
+        import hashlib
+        hashsha = hashlib.sha256(passd.replace('\n','').encode())
+        ManageSQL.addUser(user,hashsha.hexdigest(),"1","0")
+        return redirect(url_for('loginp'))
+    else:
+        return render_template('first.html')
+    
+
 
