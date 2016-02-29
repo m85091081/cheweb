d59dd651e34c7b060fc64a58aa1742fd7286b518
m85091081 <b10216041@yuntech.edu.tw>
2016-02-29 21:41:58 +0800

    new content

--- muMDAU_app/index.py
+++ muMDAU_app/index.py
@@ -4,6 +4,61 @@ import markdown ,time,os
 
 main = Blueprint('main',__name__)
 
+@main.route('/work')
+def work():
+    nowtime = time.strftime('%Y')
+    pastime = int(nowtime) - 1
+    postpath = "./work"
+    if os.path.exists(postpath) == True:
+        directory = os.path.expanduser(postpath)
+        data = []
+        content1 = []
+        i=0
+        if os.listdir(directory)==None:
+            pastdata = " "
+        else:
+            filel = os.listdir(directory)
+            filel.sort(reverse=True)
+            for f in filel:
+                if os.path.isfile(os.path.join(directory,f)):
+                    fstr = str(f).split("-",2)
+                    nowm = time.strftime('%m')
+                    if int(nowm) - 6 >=  0:
+                        if int(fstr[0]) == int(nowtime):
+                            if int(fstr[1]) - 6  <= 0:
+                                i = i+1
+                                with open("./work/"+str(f),'r') as fil:
+                                    content = fil.readline()
+                                    content = content.replace("title:","")
+                                    data.insert(i,content)
+                                    content1.insert(i,fil.read())
+                    else:
+                        if int(fstr[0]) == int(nowtime):
+                            if 6 - int(fstr[1]) >= 0:
+                                i = i+1
+                                with open("./work/"+str(f),'r') as fil:
+                                    content = fil.readline()
+                                    content = content.replace("title:","")
+                                    data.insert(i,content)
+                                    content1.insert(i,fil.read())
+                                
+                        elif int(fstr[0]) == int(nowtime)- 1:
+                            nowm = time.strftime('%m')
+                            if int(fstr[1]) - 6 - int(nowm) > 0 :
+                                i = i+1
+                                with open("./work/"+str(f),'r') as fil:
+                                    content = fil.readline()
+                                    content = content.replace("title:","")
+                                    data.insert(i,content)
+                                    content1.insert(i,fil.read())
+                            
+            pastdata = data
+            contentdata = content1
+    else:
+        os.makedirs(postpath)
+
+    return render_template('work.html',**locals())
+
 @main.route('/')
 def index():
     nowtime = time.strftime('%Y')
@@ -13,6 +68,8 @@ def index():
         directory = os.path.expanduser(postpath)
         data = []
         content1 = []
+        dataa = []
+        contentt1 = []
         i=0
         if os.listdir(directory)==None:
             pastdata = " "
@@ -32,6 +89,13 @@ def index():
                                     content = content.replace("title:","")
                                     data.insert(i,content)
                                     content1.insert(i,fil.read())
+                            else:
+                                a = a+1
+                                with open("./posts/"+str(f),'r') as filipt:
+                                    contentt = filipt.readline()
+                                    contentt = contentt.replace("title:","")
+                                    dataa.insert(i,contentt)
+                                    contentt1.insert(i,filipt.read())
                     else:
                         if int(fstr[0]) == int(nowtime):
                             if 6 - int(fstr[1]) >= 0:
@@ -41,6 +105,13 @@ def index():
                                     content = content.replace("title:","")
                                     data.insert(i,content)
                                     content1.insert(i,fil.read())
+                            else:
+                                a = a+1
+                                with open("./posts/"+str(f),'r') as filipt:
+                                    contentt = filipt.readline()
+                                    contentt = contentt.replace("title:","")
+                                    dataa.insert(i,contentt)
+                                    contentt1.insert(i,filipt.read())
                                 
                         elif int(fstr[0]) == int(nowtime)- 1:
                             nowm = time.strftime('%m')
@@ -51,9 +122,18 @@ def index():
                                     content = content.replace("title:","")
                                     data.insert(i,content)
                                     content1.insert(i,fil.read())
+                            else:
+                                a = a+1
+                                with open("./posts/"+str(f),'r') as filipt:
+                                    contentt = filipt.readline()
+                                    contentt = contentt.replace("title:","")
+                                    dataa.insert(i,contentt)
+                                    contentt1.insert(i,filipt.read())
                             
             pastdata = data
             contentdata = content1
+            olddata = dataa
+            oldcontent = contentt1
     else:
         os.makedirs(postpath)
 
